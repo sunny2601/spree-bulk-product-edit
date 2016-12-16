269892e9af8b36fe0c2837839ee9c3b67cae7411
Edwin Horneij <edwin@astekwallcovering.com>
2016-12-16 11:50:09 -0800

    Uncomment CSS install method

--- lib/generators/spree_bulk_product_edit/install/install_generator.rb
+++ lib/generators/spree_bulk_product_edit/install/install_generator.rb
@@ -8,11 +8,11 @@ module SpreeBulkProductEdit
       #   append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_bulk_product_edit\n"
       #   append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_bulk_product_edit\n"
       # end
-      #
-      # def add_stylesheets
-      #   inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_bulk_product_edit\n", :before => /\*\//, :verbose => true
-      #   inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_bulk_product_edit\n", :before => /\*\//, :verbose => true
-      # end
+
+      def add_stylesheets
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_bulk_product_edit\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_bulk_product_edit\n", :before => /\*\//, :verbose => true
+      end
 
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_bulk_product_edit'
