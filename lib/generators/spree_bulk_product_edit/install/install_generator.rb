2919d418184cec00318e6b6ad8cc254afbf8c94e
Edwin Horneij <edwin@astekwallcovering.com>
2016-12-16 17:08:01 -0800

    Remove frontend css from add_sylesheets generator method

--- lib/generators/spree_bulk_product_edit/install/install_generator.rb
+++ lib/generators/spree_bulk_product_edit/install/install_generator.rb
@@ -10,7 +10,7 @@ module SpreeBulkProductEdit
       # end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_bulk_product_edit\n", :before => /\*\//, :verbose => true
+        # inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_bulk_product_edit\n", :before => /\*\//, :verbose => true
         inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_bulk_product_edit\n", :before => /\*\//, :verbose => true
       end
 
