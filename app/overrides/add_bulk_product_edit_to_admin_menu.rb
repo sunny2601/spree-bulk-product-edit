f5125661e7804240af158f26ae542ecf72194328
Edwin Horneij <edwin@astekwallcovering.com>
2017-02-21 14:32:12 -0800

    Update original for override

--- app/overrides/add_bulk_product_edit_to_admin_menu.rb
+++ app/overrides/add_bulk_product_edit_to_admin_menu.rb
@@ -1,9 +1,9 @@
 Deface::Override.new(
-    :original => '1ff398339f1a03870dde177213dd042b67365c56',
-    :virtual_path => 'spree/layouts/admin',
-    :name => 'add_bulk_product_edit_to_admin_menu',
-    :insert_bottom => '[data-hook="admin_tabs"]',
-    :text => '<ul class="nav nav-sidebar">
-        <%= tab Spree.t(:bulk_product_edit), :url => admin_bulk_product_edits_url, :icon => \'list-alt\' %>
+    original: '9128c1b242aca5889d7c052b8579f987c7897d3c',
+    virtual_path: 'spree/layouts/admin',
+    name: 'add_bulk_product_edit_to_admin_menu',
+    insert_bottom: '[data-hook="admin_tabs"]',
+    text: '<ul class="nav nav-sidebar">
+        <%= tab Spree.t(:bulk_product_edit), url: admin_bulk_product_edits_url, icon: \'list-alt\' %>
       </ul>'
 )
